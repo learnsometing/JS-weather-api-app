ca353a432956782057845f097cb108b7a3843dac
Brian Monaccio <brianmonaccio@gmail.com>
2020-01-06 22:01:08 -0500

    Removes unused code from the starter app and adds state to APP to manage returned forecast data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { SearchForm } from "./searchForm/searchForm";
+import SearchForm from "./searchForm/searchForm";
 
 function App() {
+  const [currentForecast, setCurrentForecast] = useState(null);
+  console.log(currentForecast);
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +15,9 @@ function App() {
       <main>
         <div className="main-container">
           <h1>Weather Forecast</h1>
-          <SearchForm />
+          <SearchForm setCF={setCurrentForecast} />
+          <div id="current-forecast">
+          </div>
         </div>
       </main>
     </div>

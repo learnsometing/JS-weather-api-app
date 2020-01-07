eadb4aae6cde3a006538af25694971f05b71926b
Brian Monaccio <brianmonaccio@gmail.com>
2020-01-06 21:10:28 -0500

    Revises initial layout structure and style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { SearchForm } from "./searchForm/searchForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-container">
+          Weather
+        </div>
       </header>
+      <main>
+        <div className="main-container">
+          <h1>Weather Forecast</h1>
+          <SearchForm />
+        </div>
+      </main>
     </div>
   );
 }
